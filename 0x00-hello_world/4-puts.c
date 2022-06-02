4b58aa9b695de4522a28e65b860f169c96f7cbb2
erickadikah <erickadikah2030@gmail.com>
2022-06-02 06:10:27 -0700

    c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the strings in the puts function
+ * Description: usinguding the main function this prints
+ * "programming is like building a multilingual puzzle"
+ * return:0
+ */
+int main(void)
+{
+puts("\"programing is like building a multilingual puzzle");
+return (0);
+}
