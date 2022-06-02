197ab2082fd476e1c9cf3aa4369f287f24f6ef79
erickadikah <erickadikah2030@gmail.com>
2022-06-02 06:29:54 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * main - print the strings in the puts function
  * Description: usinguding the main function this prints
  * "programming is like building a multilingual puzzle"
- * return:0
+ * return: 0
  */
 int main(void)
 {
 puts("\"programing is like building a multilingual puzzle");
-return (0);
+return  (0);
 }
