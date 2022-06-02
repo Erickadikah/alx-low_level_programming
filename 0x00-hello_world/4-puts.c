2ea6731db6e95f41aa8f6d71d0b61f8ae126e166
erickadikah <erickadikah2030@gmail.com>
2022-06-02 07:23:58 -0700

    fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,13 @@
 /**
  * main: print the strings in the puts function
  *
- * Description: using the main function this prints
- * "programming is like building a multilingual puzzle"
- * return: 0
- */
+ 
+ **/
+
 int main(void)
 {
-	init puts("programming is like building a multilingual puzzle");
-	return (0);
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }
 
 
