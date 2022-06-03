753ee03cef356485ee9aa62e934bead0f93b7041
erickadikah <erickadikah2030@gmail.com>
2022-06-03 03:47:04 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,35 @@
-#include <stdio.h>
-#include <unistd.h>
+nclude <stdio.h>
+
 /**
- *  * main - my entry point
- *   * Description: 'and that piece of art is useful" - Dora korpar, 2015-10-19'
- *    * Return: 1
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
  *     */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+		char acharacter;
+			int ainteger;
+				long along;
+					long long alonglong;
+						float afloat;
+
+							printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+								printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+									printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+										printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+											printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
+												return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
