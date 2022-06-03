2df6d3e650f3919f6d3413b9195e2c985fea14af
erickadikah <erickadikah2030@gmail.com>
2022-06-03 03:29:41 -0700

    file c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
+
+
+
+
+
 /**
- * main - print the stringsin puts function
- * Description: using th main function this prints
- * "programming is  like building a multilingual puzzle"
- * return: 0
- */
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+
+
+
 int main(void)
+
+
+
 {
-	puts("\"programing is like building a multilingualpuzzle");
-	return (0);
+
+
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
+
+
 }
