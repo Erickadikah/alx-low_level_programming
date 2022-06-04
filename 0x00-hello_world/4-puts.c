cf7f6f1328e829e57c6e6d9ff499fd0c25091716
erickadikah <erickadikah2030@gmail.com>
2022-06-03 22:05:16 -0700

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
+
+
+
 /**
- * main-prints the phrase "programming is like building a multilingual puzzle"
- * Return: 0 if exited properly, non-zerootherwise
- */
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+
+
+
 int main(void)
+
+
+
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0)
+
+
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
+
+
 }
