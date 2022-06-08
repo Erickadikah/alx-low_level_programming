d76ce77b5bb37dd27160cd30510a9560fa52c598
erickadikah <erickadikah2030@gmail.com>
2022-06-08 02:53:30 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,14 +2,10 @@
 #define MAIN_H
 /*
  * file: main.h
- * Desc: Header file containing declarations for all functions used in the 0x02-fuctions_nested_loops directory.
+ * Desc: Header file containigdeclarations for functions
+ *	used in the 0x02-fuunctions_nested_loops directory.
  */
-
-int _putchar(char):
-	void print_alphabet(void):
-		void print_alphabet_x10(void);
-		int _islower(int c);
-		int _isalpha(int c);
-		int print_sign(int n);
-		int _abs(int);
-#endif
+int _putchar(char c);
+void print_aphabets(void);
+void print_alphabet_x10(void);
+int _islower(int c);int
