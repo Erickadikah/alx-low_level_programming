9ec432feb8fdbe6ead020eb0e4a6782b0158728d
erickadikah <erickadikah2030@gmail.com>
2022-06-09 03:43:04 -0700

    sequence

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int a = 0, b = 1, next = 0;
 	int sum = 0;
+
 	while (next < 4000000)
 	{
-		while (next < 4000000)
-			a = b;
+		next = a + b;
+		a = b;
 		b = next;
 		if (next % 2 == 0)
-			if (next % 2 == 0)
+			sum += next;
 	}
 	printf("%i\n", sum);
 	return (0);
 }
-
