84477b8b84be10bf23adfe887b773fe8839a2e4e
erickadikah <erickadikah2030@gmail.com>
2022-06-13 05:28:46 -0700

    second commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strlen - returns the length of a string..
+ * @s: value to be evaluate.
+ * Return: not.
  */
-int main(void)
-{
-	char *str;
-	int len;
 
-	str = "My first strlen!";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return (0);
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
 }
