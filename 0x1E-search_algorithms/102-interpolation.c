bb68b99db80b1713e6ca98bba829b25dedc45834
erickadikah <erickadikah2030@gmail.com>
2023-01-03 22:36:33 +0300

    moving forward

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,7 +18,7 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t i, left, right;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
 	for (left = 0, right = size - 1; right >= left;)
