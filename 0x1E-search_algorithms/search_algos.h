e1ee5de712167063bcb347b1d5de200e5852e989
erickadikah <erickadikah2030@gmail.com>
2023-01-04 23:41:39 +0300

    new

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-
+size_t min(size_t a, size_t b);
 int exponential_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
